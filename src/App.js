4b6dec31422d194e9b5738bafec8de2f411e23dd
Facundo-Martiarena <53951865+Facundo-Martiarena@users.noreply.github.com>
2021-11-18 11:27:39 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import './App.css';
 import NavBar from './components/NavBar';
-import nike from './na.png';
-import nb from './nbrc.png';
-import ad from './adidasAA5.jpg';
-import as from './asics.jpg';
 import PagPricipal from './components/PagPricipal';
 import ItemListContainer from './container/ItemListContainer';
 
 function App() {
+  
   return (
     <div className="App">
       <header className="App-header">
@@ -16,10 +13,7 @@ function App() {
       <body className="App-body">
         <PagPricipal/>
         <h1>¡NUEVOS ARRIBOS!</h1>
-        <ItemListContainer bodyPhoto={nb} marca="New Balance" modelo="RC Elite" ancho="200" precio="200" />
-        <ItemListContainer bodyPhoto={nike} marca="Nike" modelo="Air Zoom Alphafly NEXT%" ancho="197" precio="200"/>
-        <ItemListContainer bodyPhoto={ad} marca="Adidas" modelo="Adizero Adios Pro" ancho="225" precio="200"/>
-        <ItemListContainer bodyPhoto={as} marca="Asics" modelo="Kayano 26" ancho="225" precio="200"/>
+        <ItemListContainer />
       </body>
 
     </div>
